ad437798917bb13a70476d44173ccfe6bb0fbed7
Gopinath <gopinathsubramani2000@gmail.com>
2024-08-11 20:54:14 +0530

    gitignore added

---
+++ start/fork.py
@@ -0,0 +1,11 @@
+# to get python version
+import sys
+import platform
+
+# way 1 using sys
+print(sys.version)
+
+# way 2 using platform
+print(platform.python_version())
+
+print()
---
+++ start/strings.py
@@ -0,0 +1,5 @@
+motiv = "To Python, learn Python"
+
+# find substring
+print(motiv.index('Java')) # returns error if not found
+print(motiv.index('Java')) # returns -1  if not found
